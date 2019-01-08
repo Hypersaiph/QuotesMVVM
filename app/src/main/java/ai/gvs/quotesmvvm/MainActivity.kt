45565e8b9e03b52736c026f85bc68d54d7127236
Hypersaiph <mygvs.mh@gmail.com>
2019-01-08 15:11:14 -0400

    Initial Commit Baby!

---
+++ app/src/main/java/ai/gvs/quotesmvvm/MainActivity.kt
@@ -0,0 +1,19 @@
+package ai.gvs.quotesmvvm
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import ai.gvs.quotesmvvm.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+
+}
---
+++ app/src/main/java/ai/gvs/quotesmvvm/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package ai.gvs.quotesmvvm.ui.main
+
+import android.arch.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
